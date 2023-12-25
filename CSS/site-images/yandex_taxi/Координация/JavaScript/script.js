167a9bfda18b4ca8fcc9f6b68d202573bb658052
vadimpopov08 <122273142+vadimpopov08@users.noreply.github.com>
2023-12-25 21:36:41 +0500

    Add files via upload

---
+++ CSS/site-images/yandex_taxi/Координация/JavaScript/script.js
@@ -0,0 +1,8 @@
+let ul,li = document.querySelectorAll('ul>li');
+ul,li.style = `
+    list-style: none;
+`
+let nav__list = document.querySelectorAll('.nav__list');
+nav__list.style = `
+    display = 'flex';
+`
